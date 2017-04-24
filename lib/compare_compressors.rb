79d55761e4c58b7a4f8afc118d26256e162f187e
John Lees-Miller <jdleesmiller@gmail.com>
2017-04-24 08:57:32 +0100

    Move plotters to folder

--- lib/compare_compressors.rb
+++ lib/compare_compressors.rb
@@ -9,10 +9,10 @@ require_relative 'compare_compressors/cost_model'
 require_relative 'compare_compressors/result'
 require_relative 'compare_compressors/group_result'
 require_relative 'compare_compressors/costed_group_result'
+
 require_relative 'compare_compressors/plotter'
-require_relative 'compare_compressors/cost_plotter'
-require_relative 'compare_compressors/raw_plotter'
-require_relative 'compare_compressors/command_line_interface'
+require_relative 'compare_compressors/plotters/cost_plotter'
+require_relative 'compare_compressors/plotters/raw_plotter'
 
 require_relative 'compare_compressors/compressor'
 require_relative 'compare_compressors/compressors/brotli_compressor'
@@ -21,6 +21,8 @@ require_relative 'compare_compressors/compressors/gzip_compressor'
 require_relative 'compare_compressors/compressors/seven_zip_compressor'
 require_relative 'compare_compressors/compressors/xz_compressor'
 require_relative 'compare_compressors/compressors/zstd_compressor'
+
+require_relative 'compare_compressors/command_line_interface'
 
 #
 # Compare compression algorithms.
